34407f001c531dcb0aed5fadfd77be0a927f1f25
minh-bao <mbnguyen@hotmail.fr>
2021-05-10 17:35:21 +0200

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//load Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
