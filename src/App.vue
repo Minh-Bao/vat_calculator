8e4da10298ed8abe85c069e36e41b09ee84696dc
minh-bao <mbnguyen@hotmail.fr>
2021-05-10 18:13:12 +0200

    fix indentation

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="container" id="app">
-      <div class="row justify-content-center text-center">
+    <div class="container" id="app">
+        <div class="row justify-content-center text-center">
             <div class="col-6">
-                <img alt="Vue logo" src="./assets/tva.png" class="w-50 mb-4">
-                <Vat msg="Caculate VAT"/>
+                <img alt="Vue logo" src="./assets/tva.png" class="w-50 mb-4" />
+                <Vat msg="Caculate VAT" />
             </div>
-      </div>
-  </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Vat from './components/Vat.vue'
+import Vat from "./components/Vat.vue";
 
 export default {
-  name: 'App',
-  components: {
-    Vat
-  }
-}
+    name: "App",
+    components: {
+        Vat,
+    },
+};
 </script>
 
 <style>
-    body{
-        padding-top: 2rem;
-    }
+body {
+    padding-top: 2rem;
+}
 </style>
