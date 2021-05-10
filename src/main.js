495a8f5fa5ad965f93170fb79b7196b4ec39ac73
minh-bao <mbnguyen@hotmail.fr>
2021-05-10 18:25:19 +0200

    import and configure vue-currency-filter

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ Vue.config.productionTip = false
 //load Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//import vue-currency-filter
+import VueCurrencyFilter from 'vue-currency-filter';
+Vue.use(VueCurrencyFilter, {
+    symbol : '€',
+    fractionCount: 2,
+    symbolPosition: 'back'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
